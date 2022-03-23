fb053a40360fd8467509b9319a1ab96cac963b0e
Igor Zablotchi <igorz@mit.edu>
2022-03-23 18:02:27 -0400

    Added p (field size) to high-level script

--- scripts/run-fracs.sh
+++ scripts/run-fracs.sh
@@ -2,11 +2,18 @@
 # This script should be launched from the root of the repository, i.e.
 # ./scripts/run-fracs.sh
 
-fracs=($(seq 0.1 0.05 0.6))
-for frac in "${fracs[@]}"
+fracs=($(seq 0.2 0.1 0.9))
+ps=($(256 512))
+for p in "${ps[@]}"
 do
-  ./scripts/slurm-train.sh \
-    dataset.frac_train=$frac \
-    dataset=permutation_group_dataset \
-    'wandb.wandb_tags="[transformer, data_scaling, S5]"'
+  for frac in "${fracs[@]}"
+  do
+    ./scripts/slurm-train.sh \
+      dataset.frac_train=$frac \
+      dataset.p=$p \
+      'wandb.wandb_tags="[transformer, data_scaling]"'
+  done
 done
+
+
+#      dataset=permutation_group_dataset \
