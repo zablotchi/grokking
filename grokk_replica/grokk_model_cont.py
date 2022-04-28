78d32b42be176cf13010f462aaa77a586014f580
Tony Wang <twang6@mit.edu>
2022-04-27 22:45:47 -0400

    Set up model / data grokk_cont experiment
    We scale both the data_frac and the transformer width.

--- grokk_replica/grokk_model_cont.py
+++ grokk_replica/grokk_model_cont.py
@@ -10,22 +10,16 @@ from grokk_replica.utils import causal_attn_mask, parameter_norm
 class GrokkModelContOut(nn.Module):
     def __init__(self, transformer_config, vocab_size, output_size, device):
         super().__init__()
+        hidden_dim = transformer_config["hidden_dim"]
+
         self.transformer = Transformer(
-            **transformer_config, vocab_size=vocab_size, output_size=1024
+            **transformer_config, vocab_size=vocab_size, output_size=hidden_dim
         )
-        # self.cont_head = nn.Sequential(
-        #     nn.Softmax(dim=-1),
-        #     nn.Linear(in_features=output_size, out_features=output_size),
-        #     nn.ReLU(),
-        #     nn.Linear(in_features=output_size, out_features=output_size),
-        #     nn.ReLU(),
-        #     nn.Linear(in_features=output_size, out_features=1),
-        # )
         self.cont_head = nn.Sequential(
             nn.ReLU(),
-            nn.Linear(in_features=1024, out_features=1024),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(in_features=1024, out_features=1),
+            nn.Linear(hidden_dim, 1),
         )
         self.device = device
 
@@ -37,17 +31,15 @@ class GrokkModelContOut(nn.Module):
             .to(self.device)
         )
         transformer_embeddings, attns, _ = self.transformer(x, attn_mask)
-        # predictions = einops.rearrange(
-        #     self.cont_head(transformer_embeddings),
-        #     "... 1 -> ...",
-        # )
-        predictions = self.output_head(transformer_embeddings)
+        predictions = einops.rearrange(
+            self.cont_head(transformer_embeddings),
+            "... 1 -> ...",
+        )
         return predictions, attns
 
     def get_loss(self, x, y):
         predictions, attns = self(x)
         mse = F.mse_loss(predictions[:, -1], y)
-        # loss = F.l1_loss(predictions[:, -1], y)
         attn_entropies = sum(
             [
                 -(attn * torch.log(attn + 1e-7)).sum(dim=-1).mean().item()
@@ -65,14 +57,3 @@ class GrokkModelContOut(nn.Module):
             ),
             "param_norm": (param_norm, 1),
         }
-
-    def output_head(self, logits: torch.Tensor) -> torch.Tensor:
-        # return self.softmax(logits) @ (
-        #     torch.arange(logits.shape[-1]).to(self.device) / logits.shape[-1]
-        # )
-        vocab_size = logits.shape[-1]
-        return torch.einsum(
-            "bsv, v -> bs",
-            logits.softmax(dim=-1),
-            (torch.arange(vocab_size).to(self.device) / vocab_size),
-        )
--- scripts/run-fracs.sh
+++ scripts/run-fracs.sh
@@ -2,12 +2,25 @@
 # This script should be launched from the root of the repository, i.e.
 # ./scripts/run-fracs.sh
 
-fracs=($(seq 0.01 0.01 0.1) $(seq 0.12 0.02 0.3) $(seq 0.35 0.05 0.7) 0.8 0.9)
-for frac in "${fracs[@]}"
+dropouts=(0 0.1)
+width_scales=(1 4)
+fracs=($(seq 0.01 0.04 0.1) $(seq 0.12 0.08 0.3) $(seq 0.35 0.2 0.7) 0.8 0.9)
+for dropout in "${dropouts[@]}"
 do
-  ./scripts/slurm-train.sh \
-    model=grokk_model_cont_out \
-    dataset=sub_dataset_cont \
-    dataset.frac_train=$frac \
-    'wandb.wandb_tags="[transformer, data_scaling, 2_fc_layers]"'
+  for frac in "${fracs[@]}"
+  do
+    for width_scale in "${width_scales[@]}"
+    do
+      ./scripts/slurm-train.sh \
+        model=grokk_model_cont_out \
+        dataset=sub_dataset_cont \
+        dataset.frac_train=$frac \
+        model.transformer_config.heads=$(expr 4 \* $width_scale) \
+        model.transformer_config.attn_dim=32 \
+        model.transformer_config.hidden_dim=$(expr 128 \* $width_scale) \
+        model.transformer_config.intermediate_dim=$(expr 128 \* $width_scale) \
+        model.transformer_config.dropout=$dropout \
+        'wandb.wandb_tags="[cont-plateau]"'
+    done
+  done
 done
